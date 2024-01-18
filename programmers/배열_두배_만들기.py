de400ff7153256d6859011b6d9262aa7d1b4cad7
sapjil <ksy7678@gmail.com>
2024-01-18 22:54:04 +0900

    add "배열_두배_만들기.py"

---
+++ programmers/배열_두배_만들기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        answer.append(number * 2)
+    return answer
