f9a25797558570864c004eabc67eddcc36cdb518
sapjil <ksy7678@gmail.com>
2024-01-24 01:11:27 +0900

    add '피자나눠먹기.py'

--- programmers/짝수는_싫어요.py
+++ programmers/짝수는_싫어요.py
@@ -10,3 +10,20 @@ def solution(n):
 
 print(solution(10))
 print(solution(15))
+
+
+###############################
+# 다른 사람 풀이_!
+def solution(n):
+    return [i for i in range(1, n+1, 2)]
+
+
+
+# 다른 사람 풀이_2
+def solution(n):
+    return [x for x in range(n + 1) if x % 2]
+
+
+# 다른 사람 풀이_3
+def solution(n):
+    return list(range(1, n+1, 2))
