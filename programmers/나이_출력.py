fb0c6547140436f202089dd750dc8c82ec502fc7
sapjil <ksy7678@gmail.com>
2024-01-27 19:28:32 +0900

    add '나이_출력.py'

---
+++ programmers/나이_출력.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+def solution(age):
+    year = 2022
+    birth_year = year - age + 1
+    return birth_year
+
+
+
+print(solution(40))
+print(solution(23))
