5b40b47f65e48c6e429b764ef8b3806f86d8dcbd
sapjil <ksy7678@gmail.com>
2024-02-03 02:00:37 +0900

    add "편지.py"

---
+++ programmers/편지.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120898
+
+def solution(message):
+    return len(message) * 2
+
+
+print(solution("happy birthday!"))
+print(solution("I love you~"))
