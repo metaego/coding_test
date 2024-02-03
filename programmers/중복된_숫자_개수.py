51a14aad3eec8299e448b3960aad0a6b9f66bb20
sapjil <ksy7678@gmail.com>
2024-02-04 00:35:17 +0900

    add '중복된_숫자_개수.py'

---
+++ programmers/중복된_숫자_개수.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120583
+
+def solution(array, n):
+    return array.count(n)
+
+print(solution([1, 1, 2, 3, 4, 5], 1))
+print(solution([0, 2, 3, 4], 1))
