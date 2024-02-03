748352f4b7940070aadee8bd6480bc6e7552c937
sapjil <ksy7678@gmail.com>
2024-02-04 00:32:05 +0900

    add '배열_자르기.py'

---
+++ programmers/배열_자르기.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120833
+
+def solution(numbers, num1, num2):
+    return numbers[num1: num2 +1]
+
+print(solution([1, 2, 3, 4, 5], 1, 3))
+print(solution([1, 3, 5], 1, 2))
