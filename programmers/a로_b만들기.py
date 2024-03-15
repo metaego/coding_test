95a5bdc6af21908662cccb3f138ec95f96890817
sapjil <ksy7678@gmail.com>
2024-03-16 00:25:27 +0900

    update "a로_b만들기.py"

--- programmers/a로_b만들기.py
+++ programmers/a로_b만들기.py
@@ -1,9 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/120886
 
+# def solution(before, after):
+#     if ''.join(reversed(before)) == after: return 1
+#     return 0
+
 def solution(before, after):
-    print(''.join(reversed(before)))
-    if ''.join(reversed(before)) == after: return 1
-    return 0
+    return (sorted(before) == sorted(after)) * 1
+
+
 
 print(solution('olleh', 'hello'))
 print(solution('allpe', 'apple'))
