562011dc210b38783a4967f060c82e2ebbca8edb
sapjil <ksy7678@gmail.com>
2024-03-29 00:01:19 +0900

    add "외계어_사전.py"

--- programmers/외계어_사전.py
+++ programmers/외계어_사전.py
@@ -3,9 +3,19 @@
 def solution(spell, dic):
     answer = 2
     for word in dic:
-        if sorted(list(word)) in sorted(spell):
+        if set(word) == set(spell):
             answer = 1
     return answer
 
 print(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"]))
 print(solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"]))
+print(solution(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"]))
+
+###############################################################################
+# 다른 사람 풀이
+def solution(spell, dic):
+    spell = set(spell)
+    for s in dic:
+        if not spell-set(s):
+            return 1
+    return 2
